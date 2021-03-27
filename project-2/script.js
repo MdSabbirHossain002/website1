65b03002a68210d600f3131fb2570498441a0983
MdSabbirHossain002 <80336752+MdSabbirHossain002@users.noreply.github.com>
2021-03-27 19:57:35 +0600

    Add files via upload

---
+++ project-2/script.js
@@ -0,0 +1,7 @@
+function menuCheck(){
+  document.querySelector("#one").classList.toggle("check1");
+  document.querySelector("#two").classList.toggle("check2");
+  document.querySelector("#three").classList.toggle("check3");
+  document.querySelector("nav ul").classList.toggle("ul");
+  document.querySelector("nav ul li").classList.toggle("li");
+}
